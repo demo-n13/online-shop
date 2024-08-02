ffa91d8727be3aa45c2037516dc9d31e7094460a
Timurbek <timurbekbeksaburov.com>
2024-08-02 17:09:17 +0500

    issue solved

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,13 @@ app.get("/", (req, res) => {
   res.send("ok");
 });
 
+app.get('/products',(req,res)=>{
+  res.send({
+    message : 'get Products',
+    data : []
+  })
+})
+
 app.listen(3000, "localhost", () => {
   console.log(`Server listening on ${3000}`);
 });
